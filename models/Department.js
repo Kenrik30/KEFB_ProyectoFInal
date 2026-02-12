4ba01daa79bc87d67ee55929887cb3ba2f8d24fd
= <=>
2026-02-12 09:31:36 -0600

    lofica de atribituos para entidad departamento

---
+++ models/Department.js
@@ -0,0 +1,38 @@
+const mongoose = require ('mongoose');
+
+const departmentSchema = new mongoose.Schema({
+    nombre: {
+    type: String,
+    required: [true, 'El nombre es obligatorio'],
+    unique: true,
+    minlength: [3, 'El nombre del departamento debe tener al menos 3 caracteres'],
+    trim: true,
+    enum: {
+      values: [
+        'Ahuachapán', 
+        'Santa Ana', 
+        'Sonsonate', 
+        'La Libertad', 
+        'Chalatenango', 
+        'San Salvador', 
+        'Cuscatlán', 
+        'La Paz', 
+        'Cabañas', 
+        'San Vicente', 
+        'Usulután', 
+        'San Miguel', 
+        'Morazán', 
+        'La Unión'
+      ],
+      message: '{VALUE} no es un departamento válido de El Salvador'
+    }
+  },
+  descripcion: {
+    type: String,
+    required: [true, 'La descripción es necesaria'],
+    minlength: [10, 'Haz una descripción un poco más larga']
+  }
+});
+
+
+  module.exports = mongoose.model('Department', departmentSchema);
