eaac225d5a71dd059d2d8e9a4741a929e383c3f5
= <=>
2026-02-13 18:30:03 -0600

    Rutas y configuracion para registros de usuarios por su cuenta

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,11 +4,17 @@ const userController = require('../controllers/userController');
 const auth = require('../middlewares/authMiddleware');
 const checkRole = require('../middlewares/checkRoleMiddleware');
 
+//Rutas para usuarios comunes
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+
+//Rutas para administradores
 router.get('/', auth, checkRole('ADMIN_ROLE'), userController.getUsers);
 router.get('/:id', auth, userController.getUserById);
 router.post('/', auth,checkRole('ADMIN_ROLE'), userController.createUser);
-router.post('/login', userController.login);
 router.put('/:id', auth,checkRole('ADMIN_ROLE'), userController.updateUser);
 router.put('/:id/change-password', auth, userController.changePasswordUser);
+
+
 
 module.exports = router;
